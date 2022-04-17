46a2b9b9aad28e01f14e4243e88adac2d0a86997
Crackle2K <crackleontwitch@gmail.com>
2022-04-17 15:20:15 -0400

    part 2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -13,9 +16,19 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "r!ping"){
+    if (message.content == ".ping"){
         message.reply("Pong!")
     }
 })
+
+const welcomeChannelId = "954767088264814602"
+
+ client.on("guildMemberAdd", async (member) => {
+     const img = await generateImage(member)
+     member.guild.channels.cache.get(welcomeChannelId).send({
+         content: `<@${member.id}> Welcome to the server!`,
+         files: [img]
+     })
+ })
 
 client.login(process.env.TOKEN)
